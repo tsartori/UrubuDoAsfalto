3dea85e1ad46690f3d7b225e28ce42ab09655e9c
Thiago Sartori <thiagosartori@live.com>
2026-04-29 16:29:04 -0300

    update index

--- script.js
+++ script.js
@@ -26,3 +26,44 @@ const menuNav = document.getElementById("menuNav");
 menuBtn.addEventListener("click", () => {
   menuNav.classList.toggle("active");
 });
+
+// LIGHTBOX
+const imagens = document.querySelectorAll(".galeria img");
+const lightbox = document.getElementById("lightbox");
+const imgGrande = document.getElementById("img-grande");
+
+let index = 0;
+
+function mostrarImagem(i) {
+  index = (i + imagens.length) % imagens.length;
+  imgGrande.src = imagens[index].src;
+}
+
+imagens.forEach((img, i) => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "block";
+    mostrarImagem(i);
+  });
+});
+
+document.querySelector(".fechar").onclick = () => {
+  lightbox.style.display = "none";
+};
+
+document.querySelector(".esquerda").onclick = (e) => {
+  e.stopPropagation();
+  mostrarImagem(index - 1);
+};
+
+document.querySelector(".direita").onclick = (e) => {
+  e.stopPropagation();
+  mostrarImagem(index + 1);
+};
+
+document.addEventListener("keydown", (e) => {
+  if (lightbox.style.display === "block") {
+    if (e.key === "ArrowLeft") mostrarImagem(index - 1);
+    if (e.key === "ArrowRight") mostrarImagem(index + 1);
+    if (e.key === "Escape") lightbox.style.display = "none";
+  }
+});
